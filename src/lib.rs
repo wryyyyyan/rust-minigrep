bc18f6f2ae2b02f0cff824c76891b01ab46f312d
Yan <yan_sami@hotmail.com>
2020-12-20 14:13:16 -0300

    loops trocados por iteradores

--- src/lib.rs
+++ src/lib.rs
@@ -25,13 +25,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("argumentos insuficientes");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Não recebi uma frase de consulta")
+        };
         
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Não recebi um nome de arquivo"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(
@@ -44,29 +50,24 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let query = query.to_lowercase();
-    let mut results = Vec::new();
+    contents
+        .lines()
+        .filter(
+            |line| 
+            { 
+                line.to_lowercase()
+                    .contains(&query.to_lowercase()) 
+            })
+        .collect()
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
 }
 
 #[cfg(test)]
