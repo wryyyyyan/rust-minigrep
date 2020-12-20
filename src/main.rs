e0bea6c87140cf88b77d0cef9d5dbeacdc6ff9d2
Yan <yan_sami@hotmail.com>
2020-12-19 23:19:59 -0300

    Primeiro commit

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problema processando argumentos: {}", err);
+        process::exit(1);
+    });
+
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Erro na aplicação: {}", e);
+
+        process::exit(1);
+    }
+}
+
